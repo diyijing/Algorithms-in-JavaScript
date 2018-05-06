534f36b3969941816be39c5aca6409562b432e02
lichao <lichao.frontend@bytedance.com>
2018-05-06 23:54:58 +0800

    DoubleNode

--- Chapter 1/List.js
+++ Chapter 1/List.js
@@ -96,3 +96,70 @@ export class Queue extends List {
         };
     }
 }
+
+class DoubleNode {
+    constructor(item, pre = null, next = null) {
+        this.item = item;
+        this.pre = item;
+        this.next = item;
+    }
+}
+
+class DoubleList {
+    first = null;
+    last = null;
+    N = 0;
+    isEmpty() {
+        return this.N === 0;
+    }
+    /**
+     * 表头插入
+     * @param {any} item
+     */
+    unshfit(item) {
+        const node = new DoubleNode(item, null, this.first);
+        this.first = node;
+        if (this.isEmpty()) {
+            this.last = node;
+        }
+        return ++this.N;
+    }
+    /**
+     * 表位插入
+     * @param {any} item
+     */
+    push(item) {
+        const node = new DoubleNode(item, this.last, null);
+        this.last = node;
+        if (this.isEmpty()) {
+            this.first = node;
+        }
+        return ++this.N;
+    }
+    /**
+     * 表头删除
+     */
+    shift() {
+        if (this.first) {
+            const item = this.first.item;
+            this.first = this.first.next;
+            return item;
+        } else {
+            return void 0;
+        }
+    }
+    // 从表尾删除
+    pop() {
+        if (this.last) {
+            const item = this.last.item;
+            this.last = this.last.pre;
+            return item;
+        } else {
+            return void 0;
+        }
+    }
+
+    insertBefore() {}
+    insertAfter() {}
+    remove(item) {}
+}
